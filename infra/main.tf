acc9fd0229cfc95e60d22891d2ef0bd613373777
Mahesh Dodke <ezdev.mahesh@gmail.com>
2024-07-03 16:42:29 +0530

    Added code for GCP deployment using terraform

---
+++ infra/main.tf
@@ -0,0 +1,70 @@
+provider "google" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+# Function to determine the domain based on the branch
+locals {
+  domain_name = var.feature_branch != "" ? "${var.commit_hash}.${var.domain}" : var.branch == "main" ? var.domain : "${var.branch}.${var.domain}"
+}
+
+resource "google_compute_instance" "app_instance" {
+  count        = var.feature_branch != "" ? 1 : 0
+  name         = "${var.feature_branch != "" ? "feature-${var.feature_branch}-${var.commit_hash}" : var.branch}-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) \
+      stable"
+
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io
+
+    docker run -d -p 80:80 ${var.docker_image}
+  EOF
+
+  tags = ["http-server"]
+}
+
+resource "google_compute_address" "ip_address" {
+  name = "${var.branch}-ip"
+}
+
+resource "google_dns_record_set" "dns_record" {
+  name         = "${local.domain_name}."
+  type         = "A"
+  ttl          = 300
+  managed_zone = var.dns_zone
+
+  rrdatas = [google_compute_address.ip_address.address]
+}
+
+output "instance_ip" {
+  value = google_compute_address.ip_address.address
+}
---
+++ infra/variables.tf
@@ -0,0 +1,46 @@
+variable "gcp_credentials_file" {
+  description = "Path to the GCP credentials file"
+  type        = string
+}
+
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+}
+
+variable "branch" {
+  description = "Branch name"
+  type        = string
+}
+
+variable "domain" {
+  description = "Base domain"
+  type        = string
+}
+
+variable "feature_branch" {
+  description = "Feature branch name, if applicable"
+  type        = string
+  default     = ""
+}
+
+variable "commit_hash" {
+  description = "Commit hash for feature branch"
+  type        = string
+  default     = ""
+}
+
+variable "docker_image" {
+  description = "Docker image to deploy"
+  type        = string
+}
+
+variable "dns_zone" {
+  description = "DNS zone for managing records"
+  type        = string
+}
