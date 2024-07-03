24eff05ba00f176ca5c7186a36206f2cded4074c
Mahesh Dodke <ezdev.mahesh@gmail.com>
2024-07-03 16:52:52 +0530

    Updated code to send region via variables

--- infra/main.tf
+++ infra/main.tf
@@ -12,11 +12,11 @@ locals {
 resource "google_compute_instance" "app_instance" {
   count        = var.feature_branch != "" ? 1 : 0
   name         = "${var.feature_branch != "" ? "feature-${var.feature_branch}-${var.commit_hash}" : var.branch}-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-12"
     }
   }
 
